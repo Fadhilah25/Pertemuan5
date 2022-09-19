1d1d726e9155c9cba8a49f56b031280a954b54c8
FadhilahAA <112860183+Fadhilah25@users.noreply.github.com>
2022-09-19 21:54:09 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //luas lingkaran
+    float r1,jaw;
+    cout<<"\nJari-jari Lingkaran:";cin>>r1;
+    jaw=3.14*r1*r1;
+    cout << "\nLuas Lingkaran:"<<jaw;
+
+
+    //luas persegi Panjang
+    float P1,L1,hsl;
+    cout<<"\nMasukan Panjang:";cin>>P1;
+    cout<<"\nMasukan Lebar:";cin>>L1;
+    hsl=P1*L1;
+    cout<<"Luas Persegi Panjang:"<<hsl;
+
+
+    return 0;
+}
